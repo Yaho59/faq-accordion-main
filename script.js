b7cf7f9eebfb12cda285a5e06139efb64c72a245
yaoska telleria <yaoskatelleria59@gmail.com>
2023-12-10 13:27:10 -0600

    Agregando interactividad al proyecto

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const acordeonQuestions =  document.querySelectorAll(".question");
 
+acordeonQuestions.forEach(item => {
+    const buttonShow = item.querySelector('.icon-plus');
+    const buttonClose = item.querySelector('.icon-minus')
+    const paragraph = item.querySelector(".paragraph");
+
+    buttonClose.addEventListener('click', () => {
+        paragraph.style.display = 'none';
+        buttonClose.style.display = 'none';
+        buttonShow.style.display = 'block';
+    })
+
+    buttonShow.addEventListener('click', ()=>{
+        paragraph.style.display ='block';
+        buttonClose.style.display = 'block';
+        buttonShow.style.display = 'none';
+    })
+})
